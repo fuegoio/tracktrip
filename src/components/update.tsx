5eccf7e6bd8cadbdcdc30d4ed6e103b0648714d1
Alexis Tacnet <alexis@mistral.ai>
2025-09-30 10:42:12 +0200

    add install nudge

--- src/components/update.tsx
+++ src/components/update.tsx
@@ -1,5 +1,6 @@
 import { useRegisterSW } from "virtual:pwa-register/react";
 import { toast } from "sonner";
+import { useEffect, useState } from "react";
 
 export function Update() {
   const { updateServiceWorker } = useRegisterSW({
@@ -16,6 +17,25 @@ export function Update() {
         closeButton: true,
       });
     },
+  });
+
+  useEffect(() => {
+    if (/Android|iPhone/i.test(navigator.userAgent)) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      window.addEventListener("beforeinstallprompt", (e: any) => {
+        e.preventDefault();
+
+        toast("You can install this application", {
+          action: {
+            label: "Install",
+            onClick: () => {
+              e.prompt();
+            },
+          },
+          closeButton: true,
+        });
+      });
+    }
   });
 
   return null;
