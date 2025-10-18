e5cf85bf0d72c1831530bfca8ad7b03f2be3c48f
Alexis Tacnet <alexis@mistral.ai>
2025-10-18 14:01:32 +0200

    add sparkline on home screen

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,4 +1,4 @@
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import utc from "dayjs/plugin/utc";
 import weekOfYear from "dayjs/plugin/weekOfYear";
@@ -6,3 +6,19 @@ import weekOfYear from "dayjs/plugin/weekOfYear";
 dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 dayjs.extend(weekOfYear);
+
+export function getWeeksBetweenDates(startDate: Dayjs, endDate: Dayjs) {
+  const weeks: string[] = [];
+  let currentDate = dayjs(startDate);
+  const end = dayjs(endDate);
+
+  while (currentDate.isBefore(end) || currentDate.isSame(end, "day")) {
+    const week = currentDate.startOf("week").format("YYYY-MM-DD");
+    if (!weeks.includes(week)) {
+      weeks.push(week);
+    }
+    currentDate = currentDate.add(1, "day");
+  }
+
+  return weeks;
+}
--- src/routes/_authenticated/travels/$travelId/index.tsx
+++ src/routes/_authenticated/travels/$travelId/index.tsx
@@ -1,11 +1,14 @@
 import { eq, useLiveQuery } from "@tanstack/react-db";
 import { createFileRoute } from "@tanstack/react-router";
 import dayjs from "dayjs";
+import { Area, AreaChart } from "recharts";
 
 import { Budgets } from "@/components/home/budgets";
 import { Transactions } from "@/components/home/transactions";
 import { ScreenDrawer } from "@/components/layout/screen-drawer";
 import { ScreenHeader } from "@/components/layout/screen-header";
+import { ChartContainer } from "@/components/ui/chart";
+import { getWeeksBetweenDates } from "@/lib/dayjs";
 import { useTravel } from "@/lib/params";
 import { transactionsCollection } from "@/store/collections";
 
@@ -30,8 +33,30 @@ function TravelIndex() {
     0,
   );
 
-  const today = new Date();
+  function sumTransactionsByWeek() {
+    const startOfTravel = dayjs(travel.startDate).startOf("day");
+    const periodsSinceStart = getWeeksBetweenDates(startOfTravel, dayjs());
+    const result = periodsSinceStart.map((week) => ({
+      week: dayjs(week).startOf("week").format("YYYY-MM-DD"),
+      sum: 0,
+    }));
 
+    travelTransactions.forEach((transaction) => {
+      const transactionWeek = dayjs(transaction.date)
+        .startOf("week")
+        .format("YYYY-MM-DD");
+      const weekIndex = periodsSinceStart.indexOf(transactionWeek);
+      if (weekIndex !== -1) {
+        result[weekIndex]!.sum += transaction.amount;
+      }
+    });
+
+    return result;
+  }
+
+  const transactionsByPeriod = sumTransactionsByWeek();
+
+  const today = new Date();
   const todayTransactionsSum = travelTransactions
     .filter((transaction) => dayjs(transaction.date).isSame(today, "day"))
     .reduce((acc, transaction) => {
@@ -62,17 +87,53 @@ function TravelIndex() {
           What are we doing today?
         </div>
 
-        <div className="mt-6">
-          <div className="text-subtle-foreground">Total travel cost</div>
-          <div className="text-5xl text-foreground font-mono mt-1">
-            {transactionsSum.toLocaleString(undefined, {
-              style: "currency",
-              currency: travel.currency,
-            })}
+        <div className="my-2 relative z-0">
+          <div className="relative z-10 from-card bg-gradient-to-r to-transparent from-50% to-100% h-full py-4">
+            <div className="text-subtle-foreground">Total travel cost</div>
+            <div className="text-5xl text-foreground font-mono mt-1">
+              {transactionsSum.toLocaleString(undefined, {
+                style: "currency",
+                currency: travel.currency,
+              })}
+            </div>
           </div>
+
+          <ChartContainer
+            config={{
+              sum: {
+                label: "Sum",
+                color: "var(--foreground)",
+              },
+            }}
+            className="aspect-auto h-full w-1/2 absolute right-0 top-0 z-0 opacity-50"
+          >
+            <AreaChart data={transactionsByPeriod}>
+              <defs>
+                <linearGradient id="fill" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-sum)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-sum)"
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+              </defs>
+              <Area
+                dataKey="sum"
+                type="natural"
+                fill="url(#fill)"
+                stroke="var(--color-sum)"
+                stackId="a"
+              />
+            </AreaChart>
+          </ChartContainer>
         </div>
 
-        <div className="grid grid-cols-3 mt-6">
+        <div className="grid grid-cols-3">
           <div className="border-l-2 border-foreground px-4">
             <div className="text-sm text-subtle-foreground">Today cost</div>
             <div className="text-foreground font-mono font-semibold">
