3af50387977dbe8ecebe71290533ca0502d47211
Alexis Tacnet <alexis@mistral.ai>
2025-11-08 13:10:20 +0200

    fix top bar

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -7,9 +7,8 @@ import type { User } from "better-auth";
 export const TopBar = ({ travel, user }: { travel: Travel; user: User }) => {
   return (
     <div className="w-full flex items-center justify-between px-4 py-3 absolute">
-      <TravelMenu travel={travel} />
-
       <div className="absolute left-0 top-0 w-[72px] h-full bg-white/5 rounded-br-3xl z-0" />
+      <TravelMenu travel={travel} />
 
       <div className="flex items-center gap-2">
         <UserMenu user={user} />
