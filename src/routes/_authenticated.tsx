a78f3075ee8bade8f05297c3023b542d968a465c
Alexis Tacnet <alexis@mistral.ai>
2025-11-12 22:25:47 +0100

    fix dark / light theme

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/auth/cache";
 import { authClient } from "@/auth/client";
 import { ScreenLayoutProvider } from "@/components/layout/screen-layout-context";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/_authenticated")({
   component: RouteComponent,
@@ -56,6 +57,14 @@ export const Route = createFileRoute("/_authenticated")({
 });
 
 function RouteComponent() {
+  /**
+   * The documentation uses next-theme which can add a dark class to the html element.
+   * But in our case, we don't want to have a dark theme globally.
+   */
+  useEffect(() => {
+    document.documentElement.classList.remove("dark");
+  }, []);
+
   return (
     <div className="h-full w-full flex items-center justify-center sm:py-4 bg-primary">
       <main className="w-full max-w-lg mx-auto h-full relative sm:border border-white/10 sm:rounded-lg overflow-hidden sm:shadow-lg">
