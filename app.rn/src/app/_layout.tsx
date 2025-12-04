462b82414b9b8853a31a2a498da68315b2e50760
Alexis Tacnet <alexis@mistral.ai>
2025-12-04 11:46:46 +0100

    add button and configure theme

--- app.rn/src/app/_layout.tsx
+++ app.rn/src/app/_layout.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Stack } from 'expo-router';
+import { PortalHost } from '@rn-primitives/portal';
+import { ThemeProvider } from '@react-navigation/native';
+import { NAV_THEME } from '@/lib/theme';
 
 import '@/styles/global.css';
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <Stack />
+      <ThemeProvider value={NAV_THEME['dark']}>
+        <StatusBar />
+        <Stack />
+        <PortalHost />
+      </ThemeProvider>
     </>
   );
 }
--- app.rn/src/components/ScreenContent.tsx
+++ app.rn/src/components/ScreenContent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import { EditScreenInfo } from './EditScreenInfo';
+import { Button } from './ui/button';
 
 type ScreenContentProps = {
   title: string;
@@ -11,16 +12,20 @@ type ScreenContentProps = {
 
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
   return (
-    <View className={styles.container}>
+    <View className="bg-background text-foreground dark flex-1 items-center justify-center">
       <Text className={styles.title}>{title}</Text>
       <View className={styles.separator} />
       <EditScreenInfo path={path} />
       {children}
+
+      <Button>
+        <Text>Button</Text>
+      </Button>
     </View>
   );
 };
+
 const styles = {
-  container: `items-center flex-1 justify-center bg-white`,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold`,
+  title: `text-xl font-bold text-foreground`,
 };
