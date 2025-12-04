2adba3b51de36044ce1f96ea6215100a6f721df8
Alexis Tacnet <alexis@mistral.ai>
2025-12-04 10:49:26 +0100

    add expo router

---
+++ app.rn/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Stack } from 'expo-router';
+
+import '../global.css';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack />
+    </>
+  );
+}
---
+++ app.rn/app/index.tsx
@@ -0,0 +1,9 @@
+import { ScreenContent } from '../components/ScreenContent';
+
+export default function App() {
+  return (
+    <>
+      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+    </>
+  );
+}
