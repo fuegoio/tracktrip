b2bf89c01fe3f6a45495e57087abc137a074c3da
Alexis Tacnet <alexis@mistral.ai>
2025-12-04 16:26:16 +0100

    fix react-slot

--- app.rn/src/components/ScreenContent.tsx
+++ app.rn/src/components/ScreenContent.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { EditScreenInfo } from './EditScreenInfo';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Text } from '@/components/ui/text';
 
 type ScreenContentProps = {
   title: string;
@@ -12,15 +36,73 @@ type ScreenContentProps = {
 
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
   return (
-    <View className="bg-background flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center bg-background">
       <Text className={styles.title}>{title}</Text>
       <View className={styles.separator} />
       <EditScreenInfo path={path} />
       {children}
 
-      <Button>
-        <Text>Button</Text>
-      </Button>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">
+            <Text>Show Alert Dialog</Text>
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <View className="h-4" />
+          <AlertDialogFooter>
+            <AlertDialogCancel>
+              <Text>Cancel</Text>
+            </AlertDialogCancel>
+            <AlertDialogAction>
+              <Text>Continue</Text>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">
+            <Text>Open Dialog</Text>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re done.
+            </DialogDescription>
+          </DialogHeader>
+          <View className="grid gap-4">
+            <View className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" defaultValue="Pedro Duarte" />
+            </View>
+            <View className="grid gap-3">
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" defaultValue="@peduarte" />
+            </View>
+          </View>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">
+                <Text>Cancel</Text>
+              </Button>
+            </DialogClose>
+            <Button>
+              <Text>Save changes</Text>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </View>
   );
 };
