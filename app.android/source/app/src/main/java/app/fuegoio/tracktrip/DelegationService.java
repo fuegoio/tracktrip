99eb1a7788cc0cc30d499a5ca3ae7bcd7aaecef8
Alexis Tacnet <alexis@mistral.ai>
2025-12-22 10:16:46 +0100

    add android application

---
+++ app.android/source/app/src/main/java/app/fuegoio/tracktrip/DelegationService.java
@@ -0,0 +1,14 @@
+package app.fuegoio.tracktrip;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
